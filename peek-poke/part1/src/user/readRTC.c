9de37a5041202e2ba1ac08c7f47a841ccf4a4a16
Rick <vanmelisrick@gmail.com>
2019-02-22 15:19:16 +0100

    Add updates

--- peek-poke/part1/src/user/readRTC.c
+++ peek-poke/part1/src/user/readRTC.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 
-int main (int argc, char const *argv[]) {
-    // Read address 0x4002 4000
-    printf('Hello world');
+int main(int argc, char const *argv[])
+{
+    printf('Starting program');
+    const char * p = (char *)0x40024000; 
+    printf(p);
+    printf("Finished.");
     return 0;
 }
