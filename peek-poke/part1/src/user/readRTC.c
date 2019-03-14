5fe79444e59f545c8b8d02324803abae768c24ff
Rick <vanmelisrick@gmail.com>
2019-03-14 13:41:57 +0100

    Add hello world

--- peek-poke/part1/src/user/readRTC.c
+++ peek-poke/part1/src/user/readRTC.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
-    printf('Starting program');
+    printf("Starting program\n");
     const char * p = (char *)0x40024000; 
-    printf(p);
-    printf("Finished.");
+    printf("Value in memory: %d \n", *p);
+    printf("Finished.\n");
     return 0;
 }
