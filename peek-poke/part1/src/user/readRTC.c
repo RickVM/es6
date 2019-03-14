6016ba2b14f28c7708c727d2f8856339e7ae7423
Simon Lit <simon.lit@live.nl>
2019-03-14 12:15:51 +0100

    Update

--- peek-poke/part1/src/user/readRTC.c
+++ peek-poke/part1/src/user/readRTC.c
@@ -1,10 +1,14 @@
-#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
-    printf('Starting program');
-    const char * p = (char *)0x40024000; 
-    printf(p);
-    printf("Finished.");
+    printf("Starting program\n");
+    char * p = 0x400240FF; 
+    int i;
+    for (i = 0; i < 4; i++ ){
+        printf("Value of byte %d: %d\n", i ,p[i]);
+    }
+    printf("Finished.\n");
     return 0;
 }
