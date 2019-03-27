0665b9188b1d15c11f0c1e42538692a7e01313e7
Simon Lit <simon.lit@live.nl>
2019-03-27 23:27:14 +0100

    Cleanup of code

--- Pwm/src/kernel/pwm.c
+++ Pwm/src/kernel/pwm.c
@@ -1,10 +1,10 @@
-#include <linux/device.h> // Header support the kernel Driver model
-#include <linux/fs.h>     // Header for the Linux file system support
+#include <linux/device.h> 
+#include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/kdev_t.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/uaccess.h> // Header for the copy to user function
+#include <linux/uaccess.h> 
 
 #define DEVICE_NAME "pwm"
 #define CLASS_NAME "es6"
@@ -22,62 +22,59 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Rick van Melis & Simon Lit");
-MODULE_DESCRIPTION("A simple Linux char driver for the LPC3250");
-MODULE_VERSION("0.1");
+MODULE_DESCRIPTION("PWM module for the LPC3250");
+MODULE_VERSION("1.0");
 
 static int majorNumber;
+static int minor_num;
 static int numberOpens = 0;
 static struct class *pwmClass = NULL;   // Device-driver class struct pointer
 static struct device *pwmDevice = NULL; // Device-driver device struct pointer
 
-// The prototype functions for the character driver -- must come before the
-// struct defintion
 static int dev_open(struct inode *, struct file *);
 static int dev_release(struct inode *, struct file *);
 static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
 static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
 
-/*
- * The file_operations structure from /linux/fs.h lists the callback functions
- * that you wish to associated with your file operations using a C99 syntax
- * structure. char devices usually implement open, read, write and release calls
- */
 
-static int minor_num;
-
-static struct file_operations fops = {.open = dev_open,
-                                      .read = dev_read,
-                                      .write = dev_write,
-                                      .release = dev_release};
+static struct file_operations fops = {
+  .open = dev_open,
+  .read = dev_read,
+  .write = dev_write,
+  .release = dev_release
+  };
 
 static int __init pwm_init(void) {
   printk(KERN_INFO "PWM: Initializing the PWM\n");
 
-  // Try to dynamically allocate major number for the device
   majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
+  
   if (majorNumber < 0) {
     printk(KERN_ALERT "PWM: failed to register a major number\n");
   }
-  printk(KERN_INFO "PWM: registered correctly with major number %d\n",
-         majorNumber);
+
+  printk(KERN_INFO "PWM: registered correctly with major number %d\n", majorNumber);
 
   // Register the device class
   pwmClass = class_create(THIS_MODULE, CLASS_NAME);
+  
   if (IS_ERR(pwmDevice)) { // Check for error, cleanup if there is
     unregister_chrdev(majorNumber, DEVICE_NAME);
     printk(KERN_ALERT "PWM: failed to register device class\n");
     return PTR_ERR(pwmClass); // Correct way to return an error on a pointer
   }
+  
   printk(KERN_INFO "PWM: device class registered correctly\n");
 
-  pwmDevice =
-      device_create(pwmClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+  pwmDevice = device_create(pwmClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+  
   if (IS_ERR(pwmDevice)) {
     class_destroy(pwmClass);
     unregister_chrdev(majorNumber, DEVICE_NAME);
     printk(KERN_ALERT "PWM: failed to create the device\n");
     return PTR_ERR(pwmDevice);
   }
+  
   printk(KERN_INFO "PWM: device class created correctly\n");
 
   return 0;
@@ -103,79 +100,71 @@ static int dev_open(struct inode *inodep, struct file *filep) {
   return 0;
 }
 
-static ssize_t dev_read(struct file *filep, char *buffer, size_t len,
-                        loff_t *offset) {
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) {
   int error_count = 0;
   printk(KERN_INFO "PWM: Device has been read for minor num: %i", minor_num);
+  
   switch (minor_num) {
-  case pwm1_enable:
+    case pwm1_enable:
 
-    break;
+      break;
 
-  case pwm1_freq:
+    case pwm1_freq:
 
-    break;
+      break;
 
-  case pwm1_duty:
+    case pwm1_duty:
 
-    break;
-  case pwm2_enable:
+      break;
+    case pwm2_enable:
 
-    break;
+      break;
 
-  case pwm2_freq:
+    case pwm2_freq:
 
-    break;
+      break;
 
-  case pwm2_duty:
+    case pwm2_duty:
 
-    break;
+      break;
   }
   // error_count = copy_to_user(buffer, message, size_of_message);
   return 0;
 }
 
-/*
-    This function is called whenever the device is being written to from the
-   user space. The data is copied to the message[] buffer together with the
-   lenght
-*/
 static ssize_t dev_write(struct file *filep, const char *buffer, size_t len,
                          loff_t *offset) {
-  printk(KERN_INFO "PWM: Device has been written to for minor num: %i",
-         minor_num);
+  printk(KERN_INFO "PWM: Device has been written to for minor num: %i", minor_num);
   //    char temp[len];
   //    memset(temp, 0, sizeof temp);
   //    copy_from_user(temp, buffer, (unsigned long) len);
   switch (minor_num) {
-  case pwm1_enable:
+    case pwm1_enable:
 
-    break;
+      break;
 
-  case pwm1_freq:
+    case pwm1_freq:
 
-    break;
+      break;
 
-  case pwm1_duty:
+    case pwm1_duty:
 
-    break;
-  case pwm2_enable:
+      break;
+    case pwm2_enable:
 
-    break;
+      break;
 
-  case pwm2_freq:
+    case pwm2_freq:
 
-    break;
+      break;
 
-  case pwm2_duty:
+    case pwm2_duty:
 
-    break;
+      break;
   }
   return len;
 }
 
-// This function is called whenever the device is closed/released by the
-// userspace progran
 static int dev_release(struct inode *inodep, struct file *filep) {
   printk(KERN_INFO "PWM: Device is successfully closed");
   return 0;
