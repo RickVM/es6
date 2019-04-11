b2a2af62b688a7db1c204b9c37109deb91e721b3
Simon Lit <simon.lit@live.nl>
2019-04-12 01:35:16 +0200

    Add test

---
+++ gpio/Tests/play.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    int f_joy_enable;
+    int f_joy_read;
+    int f_led_enable;
+    int f_led_select;
+    int f_led_set;
+
+    printf("Starting joystick and leds test program.");
+
+
+
+
+    return 0;
+}
