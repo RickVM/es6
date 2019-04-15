691c805560493169d4b58ec52f1117bc885ebc3a
Rick <vanmelisrick@gmail.com>
2019-04-15 14:45:19 +0200

    Merge makefiles & Implement sysfs stuff

--- gpio/gpio/sys/gpio_sys.c
+++ gpio/gpio/sys/gpio_sys.c
@@ -8,7 +8,7 @@
 #define sysfs_dir "gpio"
 #define sysfs_max_data_size 1024
 
-static char[] setPin;
+static char setPin[10];
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Melis & Lit");
@@ -21,45 +21,42 @@ static ssize_t config_read(struct device *dev, struct device_attribute *attr,
   printk(KERN_INFO "sysfile_read (/sys/kernel/%s/%s) called\n", sysfs_dir,
          attr->attr.name);
   // Use set pin to return state
-  if(setPin !== NULL) {
-    
+  if(setPin != NULL) {
+    printk(setPin);
   }
   else {
-    printk("Read operation called without a set Connector + pin. Set a pin and try again.")
+    printk("Read operation called without a set Connector + pin. Set a pin and try again.\n");
   }
 
-  return 0; //RETURN SIZE OF BUFFER!!!!
+ 
+  return snprintf(buffer, sysfs_max_data_size, "Jaja\n"); //RETURN SIZE OF BUFFER!!!!
 }
 
 // Writing does not use registerCount to enable variable size writing,
 static ssize_t config_write(struct device *dev, struct device_attribute *attr,
           const char *buffer, size_t count)
 {
-  
-
   char connector[3];
   char direction[3];
   int pin;
   int result;
-
-  printk(KERN_INFO "sysfile_write (/sys/kernel/%s/%s) called, buffer: "
-      "%s, \n", sysfs_dir, attr->attr.name, buffer);
+  int used_buffer_size;
 
   // get connector, pin & direction from data. Save in global. (if not found, return error!)
   result = sscanf(buffer, "%s %d %s", connector , &pin, direction);
   if(result ==  3) { // OR result == 2? Only set connector+pin.
-    printk(KERN_INFO "Connector: %s Pin: %d Direction: %s", connector, pin, direction);
+    printk(KERN_INFO "Connector: %s Pin: %d Direction: %s\n", connector, pin, direction);
     // Search Pin & connector in pinctrl
     // check if initialized
     // [no] -> initialize
     // Set direction
   }
   else {
-    printk(KERN_WARNING "Input did not match expected format! connector pin direction e.g. j1 8 out");
+    printk(KERN_WARNING "Input did not match expected format! connector pin direction e.g. j1 8 out\n");
     result = 1;
   }
 
-  used_buffer_size = count > sysfs_max_data_size ? sysfs_max_data_size : count
+  used_buffer_size = count > sysfs_max_data_size ? sysfs_max_data_size : count;
   return used_buffer_size;
 }
 
@@ -82,27 +79,26 @@ int __init sysfs_init(void)
   gpio_obj = kobject_create_and_add(sysfs_dir, kernel_kobj);
   if (gpio_obj == NULL) {
     printk(KERN_WARNING
-        "%s module failed to load: kobject_create_and_add failed\n",
-        sysfs_data);
+        "module failed to load: kobject_create_and_add failed\n");
     return -ENOMEM;
   }
 
   result = sysfs_create_group(gpio_obj, &attr_group);
   if (result != 0) {
-    printk(KERN_WARNING "%s module failed to load: sysfs_create_group "
-         "failed with result %d\n", sysfs_data, result);
+    printk(KERN_WARNING "module failed to load: sysfs_create_group "
+         "failed with result %d\n", result);
     kobject_put(gpio_obj);
     return -ENOMEM;
   }
 
-  printk(KERN_INFO "/sys/kernel/%s/%s created\n", sysfs_dir, sysfs_data);
+  printk(KERN_INFO "/sys/kernel/%s/ created\n", sysfs_dir);
   return result;
 }
 
 void __exit sysfs_exit(void)
 {
   kobject_put(gpio_obj);
-  printk(KERN_INFO "/sys/kernel/%s/%s removed\n", sysfs_dir, sysfs_data);
+  printk(KERN_INFO "/sys/kernel/%sremoved\n", sysfs_dir);
 }
 
 module_init(sysfs_init);
